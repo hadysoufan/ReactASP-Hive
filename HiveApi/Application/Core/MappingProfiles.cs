29dc48629498c1de45d21a52169fdae3be4b15bc
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-12 21:47:20 +0200

    AutoMapper profiles for mapping between entities and DTOs

---
+++ HiveApi/Application/Core/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Core
+{
+    /// <summary>
+    /// AutoMapper profiles for mapping between entities and DTOs.
+    /// </summary>
+    public class MappingProfiles : Profile
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MappingProfiles"/> class.
+        /// </summary>
+        public MappingProfiles()
+        {
+            CreateMap<Activity, Activity>();
+        }
+    }
+}
