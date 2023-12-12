c40541724f33604337187791fa5f56d321d9ae07
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-12 21:53:26 +0200

    Represents the data context for interacting with the database

--- HiveApi/Persistence/DataContext.cs
+++ HiveApi/Persistence/DataContext.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Domain;
+using Domain.Entities;
 
 namespace Persistence
 {
+    /// <summary>
+    /// Represents the data context for interacting with the database.
+    /// </summary>
     public class DataContext : DbContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataContext"/> class with the specified options.
+        /// </summary>
+        /// <param name="options">The options for configuring the data context.</param>
         public DataContext(DbContextOptions options) : base(options)
         {
-
         }
 
         public DbSet<Activity> Activities { get; set; }
