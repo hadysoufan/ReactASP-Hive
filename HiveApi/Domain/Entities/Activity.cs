97233b9a0091cbbf661f1d4af7ade13d8f6ab00b
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-12 21:52:19 +0200

    Represents an activity entity.

--- HiveApi/Domain/Entities/Activity.cs
+++ HiveApi/Domain/Entities/Activity.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain
+﻿namespace Domain.Entities
 {
+    /// <summary>
+    /// Represents an activity entity.
+    /// </summary>
     public class Activity
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public DateTime Date{ get; set; }
+        public DateTime Date { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
         public string City { get; set; }
