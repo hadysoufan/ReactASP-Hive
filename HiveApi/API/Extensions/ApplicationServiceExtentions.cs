088fca5672907e9d71ea73f0ea5502dfc00c8c45
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-12 21:43:29 +0200

    Extension methods for configuring and adding application services to the dependency injection container

---
+++ HiveApi/API/Extensions/ApplicationServiceExtentions.cs
@@ -0,0 +1,48 @@
+﻿using Application.Activities;
+using Application.Core;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    /// <summary>
+    /// Extension methods for configuring and adding application services to the dependency injection container.
+    /// </summary>
+    public static class ApplicationServiceExtensions
+    {
+        /// <summary>
+        /// Adds application-specific services to the dependency injection container.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <param name="config">The <see cref="IConfiguration"/> used for configuration settings.</param>
+        /// <returns>The modified <see cref="IServiceCollection"/> with added services.</returns>
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(typeof(List.Handler));
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblyContaining<Create>();
+
+            return services;
+        }
+    }
+}
