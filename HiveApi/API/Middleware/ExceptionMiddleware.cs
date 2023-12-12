210f70e1cba5ed7991746cbf2b1d876d01d4d823
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-12 21:44:30 +0200

    Middleware for handling exceptions and returning a standardized JSON response

---
+++ HiveApi/API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using Application.Core;
+using System.Net;
+using System.Text.Json;
+
+namespace API.Middleware
+{
+    /// <summary>
+    /// Middleware for handling exceptions and returning a standardized JSON response.
+    /// </summary>
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExceptionMiddleware"/> class.
+        /// </summary>
+        /// <param name="next">The next middleware in the pipeline.</param>
+        /// <param name="logger">The logger.</param>
+        /// <param name="env">The hosting environment.</param>
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        /// <summary>
+        /// Invokes the middleware to handle exceptions.
+        /// </summary>
+        /// <param name="context">The HTTP context.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new AppException(context.Response.StatusCode, "Internal Server Error");
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
