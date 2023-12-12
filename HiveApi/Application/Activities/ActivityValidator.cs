0e1f0986aa61dc09543c18bef425a7a8264ae5e4
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-12 21:45:24 +0200

    Validator for the Activity Model

---
+++ HiveApi/Application/Activities/ActivityValidator.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Activities
+{
+    /// <summary>
+    /// Validator for the <see cref="Activity"/> class.
+    /// </summary>
+    public class ActivityValidator : AbstractValidator<Activity>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActivityValidator"/> class.
+        /// </summary>
+        public ActivityValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Date).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.Category).NotEmpty();
+            RuleFor(x => x.City).NotEmpty();
+            RuleFor(x => x.Venue).NotEmpty();
+        }
+    }
+}
