9c424b36794a995afbf743836e2067ee24743336
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 13:02:54 +0200

    Controller responsible for handling user profile-related operations

---
+++ HiveApi/API/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Profiles;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : BaseApiController
+    {
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetProfile(string username)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+        }
+    }
+}
