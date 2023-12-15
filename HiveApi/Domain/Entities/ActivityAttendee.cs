48d10d4b098b8d8ae29d85a11a4dc6b9eba2b22e
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:25:39 +0200

    Represents an attendee of an activity

---
+++ HiveApi/Domain/Entities/ActivityAttendee.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents an attendee of an activity.
+    /// </summary>
+    public class ActivityAttendee
+    {
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public Guid ActivityId { get; set; }
+        public Activity Activity { get; set; }
+        public bool isHost { get; set; }    
+    }
+}
