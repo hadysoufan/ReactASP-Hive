62dae14984fe0334d0b060e7a407bdeeb4fc29fd
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 15:57:48 +0200

    Represents the functionality to edit a post

---
+++ HiveApi/Application/Posts/PostEdit.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Persistence;
+
+namespace Application.Posts
+{
+    /// <summary>
+    /// Represents the functionality to edit a post.
+    /// </summary>
+    public class PostEdit
+    {
+        /// <summary>
+        /// Represents the command to edit a post.
+        /// </summary>
+        public class Command : IRequest
+        {
+            /// <summary>
+            /// Gets or sets the post with updated information.
+            /// </summary>
+            public Post Post { get; set; }
+        }
+
+        /// <summary>
+        /// Represents the handler for editing a post.
+        /// </summary>
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Handler"/> class.
+            /// </summary>
+            /// <param name="context">The data context.</param>
+            /// <param name="mapper">The AutoMapper instance.</param>
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            /// <summary>
+            /// Handles the editing of a post.
+            /// </summary>
+            /// <param name="request">The edit post command.</param>
+            /// <param name="cancellationToken">The cancellation token.</param>
+            /// <returns>A task representing the asynchronous operation.</returns>
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var post = await _context.Posts.FindAsync(request.Post.Id);
+
+                _mapper.Map(request.Post, post);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
