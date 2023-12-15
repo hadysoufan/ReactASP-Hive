b44dc770630c2a73d835e65693ead9d23da2e479
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:13:16 +0200

    Data transfer object for user information

---
+++ HiveApi/API/DTOs/UserDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace API.DTOs
+{
+    /// <summary>
+    /// Data transfer object for user information.
+    /// </summary>
+    public class UserDTO
+    {
+        /// <summary>
+        /// Gets or sets the display name of the user.
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the authentication token associated with the user.
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image associated with the user.
+        /// </summary>
+        public string Image { get; set; }
+
+        /// <summary>
+        /// Gets or sets the username of the user.
+        /// </summary>
+        public string Username { get; set; }
+    }
+}
