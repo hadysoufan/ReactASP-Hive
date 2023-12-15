f45a5d8247c70573a8fb1e17cb59341b97cdb96a
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:24:02 +0200

    Interface for accessing user-related information

---
+++ HiveApi/Application/Interface/IUserAccessor.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Interface
+{
+    /// <summary>
+    /// Interface for accessing user-related information.
+    /// </summary>
+    public interface IUserAccessor
+    {
+        /// <summary>
+        /// Gets the username of the current user.
+        /// </summary>
+        /// <returns>The username of the current user.</returns>
+        string GetUsername();
+    }
+}
