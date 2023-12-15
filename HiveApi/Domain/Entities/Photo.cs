0ff9dbee2664d95d7ae267e1e6590581854f5eee
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 13:14:26 +0200

    Represents an photo entity

---
+++ HiveApi/Domain/Entities/Photo.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents an photo entity.
+    /// </summary>
+    public class Photo
+    {
+        public string Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
