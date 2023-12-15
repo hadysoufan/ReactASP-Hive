462c58e6653aa22faf3ad8c51f5c638d9d95ebe9
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:20:17 +0200

    Determine the host and the attendees for a particular activity

--- HiveApi/Application/Activities/Details.cs
+++ HiveApi/Application/Activities/Details.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Core;
-using Domain.Entities;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -13,7 +15,7 @@ namespace Application.Activities
         /// <summary>
         /// Represents the query to retrieve details of an activity.
         /// </summary>
-        public class Query : IRequest<Result<Activity>>
+        public class Query : IRequest<Result<ActivityDTO>>
         {
             /// <summary>
             /// Gets or sets the ID of the activity for which details are requested.
@@ -24,17 +26,19 @@ namespace Application.Activities
         /// <summary>
         /// Handler for the <see cref="Query"/> to retrieve details of an activity.
         /// </summary>
-        public class Handler : IRequestHandler<Query, Result<Activity>>
+        public class Handler : IRequestHandler<Query, Result<ActivityDTO>>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
             /// <summary>
             /// Initializes a new instance of the <see cref="Handler"/> class.
             /// </summary>
             /// <param name="context">The data context.</param>
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
 
             /// <summary>
@@ -43,11 +47,13 @@ namespace Application.Activities
             /// <param name="request">The query to retrieve details of an activity.</param>
             /// <param name="cancellationToken">The cancellation token.</param>
             /// <returns>A result containing the details of the activity.</returns>
-            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<ActivityDTO>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Id);
+                var activity = await _context.Activities
+                    .ProjectTo<ActivityDTO>(_mapper.ConfigurationProvider)
+                    .FirstOrDefaultAsync(x => x.Id == request.Id);
 
-                return Result<Activity>.Success(activity);
+                return Result<ActivityDTO>.Success(activity);
             }
         }
     }
