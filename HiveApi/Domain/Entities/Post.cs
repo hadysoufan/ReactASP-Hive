58fac83cdb53df8a4084fad000888ca656a4b254
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 15:58:24 +0200

    Represents an post entity

---
+++ HiveApi/Domain/Entities/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents an post entity.
+    /// </summary>
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Image { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
