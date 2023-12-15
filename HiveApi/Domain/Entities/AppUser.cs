3793487623ff563cb6b1eb05a701ceec9c82de40
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:25:52 +0200

    Represents an application user

---
+++ HiveApi/Domain/Entities/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents an application user.
+    /// </summary>
+    public class AppUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of activities associated with the user.
+        /// </summary>
+        public ICollection<ActivityAttendee> Activities { get; set; }
+    }
+}
