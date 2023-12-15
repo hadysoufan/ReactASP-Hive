c4f68289039965f1a8ccc56cc861e20251040e0d
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:20:51 +0200

    Represents a query request to retrieve a list of activities

--- HiveApi/Application/Activities/List.cs
+++ HiveApi/Application/Activities/List.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -14,22 +16,24 @@ namespace Application.Activities
         /// <summary>
         /// Represents a query request to retrieve a list of activities.
         /// </summary>
-        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Query : IRequest<Result<List<ActivityDTO>>> { }
 
         /// <summary>
         /// Represents a handler for the <see cref="Query"/> request.
         /// </summary>
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDTO>>>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
             /// <summary>
             /// Initializes a new instance of the <see cref="Handler"/> class with the specified data context.
             /// </summary>
             /// <param name="context">The data context.</param>
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
 
             /// <summary>
@@ -38,9 +42,13 @@ namespace Application.Activities
             /// <param name="request">The query request.</param>
             /// <param name="cancellationToken">The cancellation token.</param>
             /// <returns>A result containing the list of activities.</returns>
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDTO>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
+                var activities = await _context.Activities
+                    .ProjectTo<ActivityDTO>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<ActivityDTO>>.Success(activities);
             }
         }
     }
