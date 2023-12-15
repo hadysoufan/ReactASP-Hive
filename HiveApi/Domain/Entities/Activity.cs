b81aaab9ed83f41a717edc3f42cc743a048a94a2
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:25:28 +0200

    Adding a collection of attendees for the activity

--- HiveApi/Domain/Entities/Activity.cs
+++ HiveApi/Domain/Entities/Activity.cs
@@ -12,5 +12,11 @@
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+        public bool IsCancelled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of attendees for the activity.
+        /// </summary>
+        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
     }
 }
