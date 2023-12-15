c1de24e9e608e057ad28c7748e7cd983c0f2c95a
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:28:34 +0200

    Adding configs for relation between activity and user

--- HiveApi/Persistence/DataContext.cs
+++ HiveApi/Persistence/DataContext.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Domain.Entities;
+﻿using Domain.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
@@ -17,20 +17,34 @@ namespace Persistence
         {
         }
 
+        /// <summary>
+        /// Gets or sets the DbSet for activities.
+        /// </summary>
         public DbSet<Activity> Activities { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DbSet for activity attendees.
+        /// </summary>
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
 
+        /// <summary>
+        /// Configures the relationships and keys for the entities.
+        /// </summary>
+        /// <param name="builder">The model builder.</param>
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+            // Configure the primary key for the ActivityAttendee entity
             builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
 
+            // Configure the relationship between ActivityAttendee and AppUser
             builder.Entity<ActivityAttendee>()
                 .HasOne(u => u.AppUser)
                 .WithMany(a => a.Activities)
                 .HasForeignKey(aa => aa.AppUserId);
 
+            // Configure the relationship between ActivityAttendee and Activity
             builder.Entity<ActivityAttendee>()
                 .HasOne(u => u.Activity)
                 .WithMany(a => a.Attendees)
