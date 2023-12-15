87fece726c0f659440e9d47a23be7833a20ed4c6
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 15:57:57 +0200

    Represents the functionality to retrieve a list of posts

---
+++ HiveApi/Application/Posts/PostList.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Posts
+{
+    /// <summary>
+    /// Represents the functionality to retrieve a list of posts.
+    /// </summary>
+    public class PostList
+    {
+        /// <summary>
+        /// Represents the query to retrieve a list of posts.
+        /// </summary>
+        public class Query : IRequest<List<Post>> { }
+
+        /// <summary>
+        /// Represents the handler for retrieving a list of posts.
+        /// </summary>
+        public class Handler : IRequestHandler<Query, List<Post>>
+        {
+            private readonly DataContext _context;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Handler"/> class.
+            /// </summary>
+            /// <param name="context">The data context.</param>
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            /// <summary>
+            /// Handles the retrieval of a list of posts.
+            /// </summary>
+            /// <param name="request">The query to retrieve the list of posts.</param>
+            /// <param name="cancellationToken">The cancellation token.</param>
+            /// <returns>A task representing the asynchronous operation and returning the list of posts.</returns>
+            public async Task<List<Post>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Posts.ToListAsync();
+            }
+        }
+    }
+}
