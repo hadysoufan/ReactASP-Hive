f3d4ef30f4421ecd2a809712f8618b211b0c5e04
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 13:08:33 +0200

    Represents data transfer object for attendees of an activity

--- HiveApi/Application/Activities/AttendeeDTO.cs
+++ HiveApi/Application/Activities/AttendeeDTO.cs
@@ -2,6 +2,9 @@
 {
     public  class AttendeeDTO
     {
+        /// <summary>
+        /// Represents data transfer object (DTO) for attendees of an activity.
+        /// </summary>
         public string Username { get; set; }
         public string DisplayName { get; set; }
         public string Bio { get; set; }
