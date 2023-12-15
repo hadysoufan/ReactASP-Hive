d1fcbe23a9589c4be428a82864e13406db95d0b2
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 13:11:00 +0200

    Represents the result of a photo upload operation

---
+++ HiveApi/Application/Photos/PhotoUploadResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Application.Photos
+{
+    /// <summary>
+    /// Represents the result of a photo upload operation.
+    /// </summary>
+    public class PhotoUploadResult
+    {
+        /// <summary>
+        /// Gets or sets the public ID associated with the uploaded photo.
+        /// </summary>
+        public string PublicId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL of the uploaded photo.
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
