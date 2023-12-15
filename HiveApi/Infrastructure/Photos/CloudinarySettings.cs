cedd2cc04b4443d93bf1ed1660fadc6dabe873c8
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 13:15:24 +0200

    Represents the configuration settings for connecting to Cloudinary

---
+++ HiveApi/Infrastructure/Photos/CloudinarySettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Photos
+{
+    /// <summary>
+    /// Represents the configuration settings for connecting to Cloudinary.
+    /// </summary>
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string Apikey { get; set; }
+        public string ApiSecret { get; set; }       
+    }
+}
