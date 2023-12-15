4fbcb8d2fd086bb9483ad30b66df0b3b8d274c79
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:17:24 +0200

    Service for handling JWT token creation

---
+++ HiveApi/API/Services/TokenService.cs
@@ -0,0 +1,56 @@
+﻿using Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Services
+{
+    /// <summary>
+    /// Service for handling JWT token creation.
+    /// </summary>
+    public class TokenService
+    {
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TokenService"/> class.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        /// <summary>
+        /// Creates a JWT token for the specified user.
+        /// </summary>
+        /// <param name="user">The user for whom the token is created.</param>
+        /// <returns>The generated JWT token.</returns>
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
