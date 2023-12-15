78c1811749fdb5b151cd324df81b2ade2f9eda72
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 13:12:00 +0200

    Represents the "Set Main" functionality for photos

---
+++ HiveApi/Application/Photos/SetMain.cs
@@ -0,0 +1,75 @@
+﻿using Application.Core;
+using Application.Interface;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Photos
+{
+    /// <summary>
+    /// Represents the "Set Main" functionality for photos.
+    /// </summary>
+    public class SetMain
+    {
+        /// <summary>
+        /// Represents the command to set a photo as the main photo.
+        /// </summary>
+        public class Command : IRequest<Result<Unit>>
+        {
+            /// <summary>
+            /// Gets or sets the ID of the photo to be set as the main photo.
+            /// </summary>
+            public string Id { get; set; }
+        }
+
+        /// <summary>
+        /// Handler for the <see cref="Command"/> to set a photo as the main photo.
+        /// </summary>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Handler"/> class.
+            /// </summary>
+            /// <param name="context">The data context.</param>
+            /// <param name="userAccessor">The user accessor service.</param>
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _context = context;
+                _userAccessor = userAccessor;
+            }
+
+            /// <summary>
+            /// Handles the <see cref="Command"/> to set a photo as the main photo.
+            /// </summary>
+            /// <param name="request">The command to set a photo as the main photo.</param>
+            /// <param name="cancellationToken">The cancellation token.</param>
+            /// <returns>A result indicating success or failure.</returns>
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.Include(p => p.Photos)
+                    .FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                if (user is null) return null;
+
+                var photo = user.Photos.FirstOrDefault(x => x.Id == request.Id);
+
+                if (photo is null) return null;
+
+                var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+
+                if (currentMain != null) currentMain.IsMain = false;
+
+                photo.IsMain = true;
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Result<Unit>.Success(Unit.Value);
+
+                return Result<Unit>.Failure("Problem setting main photo");
+            }
+        }
+    }
+}
