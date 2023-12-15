5becf740839783de1c67ccbe3f4f3bd86ebcff7e
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 13:02:01 +0200

    Controller responsible for handling photo-related operations

---
+++ HiveApi/API/Controllers/PhotoController.cs
@@ -0,0 +1,37 @@
+﻿using Application.Photos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// Controller responsible for handling photo-related operations.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : BaseApiController
+    {
+        /// <inheritdoc />
+        // Post: api/photo
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm] Add.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
+        }
+
+        /// <inheritdoc />
+        // Post: api/photo/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            return HandleResult(await Mediator.Send(new Delete.Command{ Id = id}));
+        }
+
+        /// <inheritdoc />
+        // Post: api/photo/{id}/setMain
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMain(string id)
+        {
+            return HandleResult(await Mediator.Send(new SetMain.Command { Id = id }));
+        }
+    }
+}
