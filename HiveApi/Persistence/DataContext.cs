518321044a0455e53e48b555ed81c25e5b0c0a9c
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:28:09 +0200

    Adding configs for relation between activity and user

--- HiveApi/Persistence/DataContext.cs
+++ HiveApi/Persistence/DataContext.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Persistence
 {
     /// <summary>
     /// Represents the data context for interacting with the database.
     /// </summary>
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="DataContext"/> class with the specified options.
@@ -17,5 +18,23 @@ namespace Persistence
         }
 
         public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.AppUser)
+                .WithMany(a => a.Activities)
+                .HasForeignKey(aa => aa.AppUserId);
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.Activity)
+                .WithMany(a => a.Attendees)
+                .HasForeignKey(aa => aa.ActivityId);
+        }
     }
 }
