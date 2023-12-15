7935692ee131be87990b34b165d05f328dbd96c8
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:13:07 +0200

    Data transfer object for user registration information

---
+++ HiveApi/API/DTOs/RegisterDTO.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    /// <summary>
+    /// Data transfer object for user registration information.
+    /// </summary>
+    public class RegisterDTO
+    {
+        /// <summary>
+        /// Gets or sets the email address of the user.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password of the user.
+        /// </summary>
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must contain at least 1 uppercase letter and 1 digit.")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display name of the user.
+        /// </summary>
+        [Required]
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the username of the user.
+        /// </summary>
+        [Required]
+        public string Username { get; set; }
+    }
+}
