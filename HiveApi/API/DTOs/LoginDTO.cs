6c06293964ffd1242232f0dd7750658006dd0026
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-15 10:12:54 +0200

    Data transfer object for user login information

---
+++ HiveApi/API/DTOs/LoginDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace API.DTOs
+{
+    /// <summary>
+    /// Data transfer object for user login information.
+    /// </summary>
+    public class LoginDTO
+    {
+        /// <summary>
+        /// Gets or sets the email address of the user.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password of the user.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
