37930b3e993150c28210dcaad918e96ecbb2ebbf
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-16 08:54:20 +0200

    Adding activities route

--- HiveClient/src/App.js
+++ HiveClient/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Home from './screens/HomeScreens/Home.screen';
@@ -8,6 +7,7 @@ import Hive from './screens/HiveScreens/Hive.screen';
 import UserProfile from './screens/HiveScreens/UserProfile/UserProfile.screen';
 import theme from './style/theme';
 import EditProfile from './screens/HiveScreens/EditUserProfile/EditProfile.screen';
+import Activities from './screens/HiveScreens/Activities/Activities.screen.tsx'
 
 function App() {
   return (
@@ -22,6 +22,8 @@ function App() {
 
         <Route path='/hive/user-profile' element={<UserProfile/>} />
         <Route path='/hive/user-profile/edit' element={<EditProfile />} />
+
+        <Route path='/hive/activities' element={<Activities />} />
       </Routes>
     </Router>
     </ThemeProvider>
