cc158ccae8d55387ba566a6fa7a22ca204487c01
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-22 20:52:12 +0200

    Controller for managing followers

---
+++ HiveApi/API/Controllers/FollowController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Followers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class FollowController : BaseApiController
+    {
+        [HttpPost("{username}")]
+        public async Task<IActionResult> Follow(string username)
+        {
+            return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
+        }
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetFollowing(string username, string predicate)
+        {
+            return HandleResult(await Mediator.Send(new FollowersList.Query { Username = username, Predicate = predicate }));
+        }
+    }
+}
