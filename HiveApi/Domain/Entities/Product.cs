01e11b8b62d88d25a497d07086a987e897abaec5
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-22 21:23:22 +0200

    Represents a product entity

---
+++ HiveApi/Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents a product entity.
+    /// </summary>
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; }
+        public string Image { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public int QtyInStock { get; set; }
+
+    }
+}
