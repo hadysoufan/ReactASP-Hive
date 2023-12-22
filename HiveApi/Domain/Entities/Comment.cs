066dbf45979321253a0354966ede0c883251da73
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-22 21:22:05 +0200

    Represents a comment entity

---
+++ HiveApi/Domain/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents a comment entity.
+    /// </summary>
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public AppUser Author { get; set; }
+        public Activity Activity { get; set; }
+        public Guid ActivityId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
