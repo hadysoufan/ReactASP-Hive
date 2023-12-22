43b5873355671d1a72ff295cf98f29c4169f2d09
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-22 21:02:23 +0200

    Represents a Data Transfer Object (DTO) for comments

---
+++ HiveApi/Application/Comments/CommentDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Comments
+{
+    /// <summary>
+    /// Represents a Data Transfer Object (DTO) for comments.
+    /// </summary>
+    public class CommentDTO
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Body { get; set; }
+        public string Username { get; set; }
+        public string  DisplayName { get; set; }
+        public string Image { get; set; }
+    }
+}
