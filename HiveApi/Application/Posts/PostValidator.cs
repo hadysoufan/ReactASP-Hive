e3bb5cc2f8c3355405cf0fd8f2997f7213f47002
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-22 21:20:53 +0200

    Represents a validator for the Post entity

---
+++ HiveApi/Application/Posts/PostValidator.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Posts
+{
+    /// <summary>
+    /// Represents a validator for the <see cref="Post"/> entity.
+    /// </summary>
+    public class PostValidator : AbstractValidator<Post>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PostValidator"/> class.
+        /// </summary>
+        public PostValidator()
+        {
+            /// <summary>
+            /// Validates that the image property of the post is not empty.
+            /// </summary>
+            RuleFor(x => x.Image).NotEmpty();
+
+            /// <summary>
+            /// Validates that the description property of the post is not empty.
+            /// </summary>
+            RuleFor(x => x.Description).NotEmpty();
+        }
+    }
+}
