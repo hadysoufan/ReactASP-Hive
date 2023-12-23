204e5873c1aca8feaae0c8262f40912960957d9f
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-23 20:59:06 +0200

    Adding react-toastify import

--- HiveClient/src/index.js
+++ HiveClient/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StoreContext, store } from "./app/stores/store.ts";
+import 'react-toastify/dist/ReactToastify.min.css';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StoreContext.Provider value={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </StoreContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
