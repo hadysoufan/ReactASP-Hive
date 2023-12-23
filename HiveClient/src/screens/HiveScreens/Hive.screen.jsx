d51be5018329aafbb3575865054d46eadd945b0d
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-23 20:56:04 +0200

    React component representing the main Hive page.

--- HiveClient/src/screens/HiveScreens/Hive.screen.jsx
+++ HiveClient/src/screens/HiveScreens/Hive.screen.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import HiveNav from '../../components/HiveComponents/HiveNavBar/HiveNav.component'
+import React, { useEffect } from 'react';
+import HiveNav from '../../components/HiveComponents/HiveNavBar/HiveNav.component.tsx'
 import LeftSection from '../../components/HiveComponents/LeftSection/LeftSection.component'
 import MiddleSection from '../../components/HiveComponents/MiddleSection/MiddleSection.component'
 import RightSection from '../../components/HiveComponents/RightSection/RightSection.component'
 import '../../asset/css/hive.styles.scss'
 import HiveGlobal from '../../styled/HiveGlobal.styled';
+import { useStore } from '../../app/stores/store.ts';
+import { observer } from 'mobx-react-lite';
 
-
+/**
+ * React component representing the main Hive page.
+ * @function Hive
+ * @returns {JSX.Element} The JSX representation of the Hive page.
+ */
 function Hive() {
+
+  const {commonStore, userStore} = useStore();
+
+  useEffect(() => {
+    if(commonStore.token){
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore])
+
+
   return (
     <>
     <HiveGlobal />
@@ -24,4 +42,4 @@ function Hive() {
   )
 }
 
-export default Hive
+export default observer(Hive);
