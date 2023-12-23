0cc74c4dde5515c5a75e860c8b1ff2f4b7b059e6
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-23 20:52:41 +0200

    Interface representing the combined stores available in the application

---
+++ HiveClient/src/app/stores/store.ts
@@ -0,0 +1,44 @@
+import { createContext, useContext } from "react";
+import ActivityStore from "./activityStore.ts";
+import UserStore from "./userStore.ts";
+import CommonStore from "./commonStore.ts";
+
+/**
+ * Interface representing the combined stores available in the application.
+ * @interface Store
+ * @property {ActivityStore} activityStore - The MobX store managing activities.
+ * @property {CommonStore} commonStore - The MobX store managing common application state.
+ * @property {UserStore} userStore - The MobX store managing user-related state.
+ */
+interface Store {
+  activityStore: ActivityStore;
+  commonStore: CommonStore;
+  userStore: UserStore;
+}
+
+/**
+ * Object containing instances of MobX stores.
+ * @const store
+ * @type {Store}
+ */
+export const store: Store = {
+  activityStore: new ActivityStore(),
+  commonStore: new CommonStore(),
+  userStore: new UserStore(),
+};
+
+/**
+ * React context for providing access to MobX stores throughout the application.
+ * @const StoreContext
+ * @type {React.Context<Store>}
+ */
+export const StoreContext = createContext(store);
+
+/**
+ * Custom hook for accessing the MobX stores within a functional component.
+ * @function useStore
+ * @returns {Store} The combined MobX stores.
+ */
+export function useStore() {
+  return useContext(StoreContext);
+}
