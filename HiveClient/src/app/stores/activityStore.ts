a77356337abd6018ff4d29d38a6d127ee43a1942
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-23 20:49:39 +0200

    MobX store for managing activities

---
+++ HiveClient/src/app/stores/activityStore.ts
@@ -0,0 +1,114 @@
+import { makeAutoObservable } from "mobx";
+import { Activity } from "../models/activity";
+import agent from "../api/agent.ts";
+
+/**
+ * MobX store for managing activities.
+ * @class ActivityStore
+ */
+class ActivityStore {
+  /**
+   * Array of activities.
+   * @type {Activity[]}
+   */
+  activities: Activity[] = [];
+
+  /**
+   * Currently selected activity.
+   * @type {Activity | undefined}
+   */
+  selectedActivity: Activity | undefined = undefined;
+
+  /**
+   * Flag indicating whether the store is in edit mode.
+   * @type {boolean}
+   */
+  editMode = false;
+
+  /**
+   * Flag indicating whether activities are currently being loaded.
+   * @type {boolean}
+   */
+  loading = false;
+
+  /**
+   * Flag indicating whether the initial loading of activities is in progress.
+   * @type {boolean}
+   */
+  loadingInitial = false;
+
+  /**
+   * Creates an instance of ActivityStore.
+   * @memberof ActivityStore
+   */
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  /**
+   * Asynchronously loads activities from the backend.
+   * @memberof ActivityStore
+   * @async
+   * @function
+   */
+  loadActivities = async () => {
+    this.setLoadingInitial(true);
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach((activity) => {
+        activity.date = activity.date.split("T")[0];
+        this.activities.push(activity);
+      });
+      this.setLoadingInitial(false);
+    } catch (error) {
+      console.log(error);
+      this.setLoadingInitial(false);
+    }
+  };
+
+  /**
+   * Sets the loadingInitial state.
+   * @memberof ActivityStore
+   * @param {boolean} state - The new loadingInitial state.
+   */
+  setLoadingInitial = (state: boolean) => {
+    this.loadingInitial = state;
+  };
+
+  /**
+   * Selects an activity by its ID.
+   * @memberof ActivityStore
+   * @param {string} id - The ID of the activity to be selected.
+   */
+  selectActivity = (id: string) => {
+    this.selectedActivity = this.activities.find((a) => a.id === id);
+  };
+
+  /**
+   * Cancels the selection of the currently selected activity.
+   * @memberof ActivityStore
+   */
+  cancelSelectedActivity = () => {
+    this.selectedActivity = undefined;
+  };
+
+  /**
+   * Opens the activity form for either editing an existing activity or creating a new one.
+   * @memberof ActivityStore
+   * @param {string | undefined} id - The ID of the activity to be edited, or undefined for creating a new activity.
+   */
+  openForm = (id?: string) => {
+    id ? this.selectActivity(id) : this.cancelSelectedActivity();
+    this.editMode = true;
+  };
+
+  /**
+   * Closes the activity form.
+   * @memberof ActivityStore
+   */
+  closeForm = () => {
+    this.editMode = false;
+  };
+}
+
+export default ActivityStore;
