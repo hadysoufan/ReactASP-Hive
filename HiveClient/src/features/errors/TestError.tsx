ade816e234af13ff4a38a0a6c346978cb3ba4b78
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-24 20:29:17 +0200

    React component for testing different error scenarios

---
+++ HiveClient/src/features/errors/TestError.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Button, Header, Segment} from "semantic-ui-react";
+import axios from 'axios';
+
+/**
+ * React component for testing different error scenarios.
+ * @function TestErrors
+ * @returns {JSX.Element} - TestErrors component
+ */
+
+export default function TestErrors() {
+    const baseUrl = 'http://localhost:5000/api/'
+
+    function handleNotFound() {
+        axios.get(baseUrl + 'buggy/not-found').catch(err => console.log(err.response));
+    }
+
+    function handleBadRequest() {
+        axios.get(baseUrl + 'buggy/bad-request').catch(err => console.log(err.response));
+    }
+
+    function handleServerError() {
+        axios.get(baseUrl + 'buggy/server-error').catch(err => console.log(err.response));
+    }
+
+    function handleUnauthorised() {
+        axios.get(baseUrl + 'buggy/unauthorised').catch(err => console.log(err.response));
+    }
+
+    function handleBadGuid() {
+        axios.get(baseUrl + 'activities/notaguid').catch(err => console.log(err.response));
+    }
+
+    function handleValidationError() {
+        axios.post(baseUrl + 'activities', {}).catch(err => console.log(err.response));
+    }
+
+    return (
+        <>
+            <Header as='h1' content='Test Error component' />
+            <Segment>
+                <Button.Group widths='7'>
+                    <Button onClick={handleNotFound} content='Not Found' basic primary />
+                    <Button onClick={handleBadRequest} content='Bad Request' basic primary />
+                    <Button onClick={handleValidationError} content='Validation Error' basic primary />
+                    <Button onClick={handleServerError} content='Server Error' basic primary />
+                    <Button onClick={handleUnauthorised} content='Unauthorised' basic primary />
+                    <Button onClick={handleBadGuid} content='Bad Guid' basic primary />
+                </Button.Group>
+            </Segment>
+        </>
+    )
+}
+
