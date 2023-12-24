5b20eed1a4def600cedb10c4c14209077170fd0f
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-24 20:21:13 +0200

    Interface representing a post object

---
+++ HiveClient/src/app/models/post.ts
@@ -0,0 +1,14 @@
+/**
+ * Interface representing a post object.
+ * @interface Post
+ * @property {string} id - The unique identifier for the post.
+ * @property {string} image - The URL of the image associated with the post.
+ * @property {string} date - The date when the post was created.
+ * @property {string} description - The description or caption of the post.
+ */
+export interface Post {
+  id: string;
+  image: string;
+  date: string;
+  description: string;
+}
