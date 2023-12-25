58d020ce1c350c8a8057762407f8069ab664f85f
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-25 19:29:13 +0200

    Editing

--- HiveClient/src/app/api/agent.ts
+++ HiveClient/src/app/api/agent.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { Activity } from "../models/activity";
 import { User, UserFormValues } from "../models/user";
 import { toast } from "react-toastify";
-import { store } from '../stores/store.ts'
+import { store } from "../stores/store.ts";
 import { Post } from "../models/post.ts";
 
 /**
@@ -16,15 +16,17 @@ const sleep = (delay: number): Promise<void> => {
   });
 };
 
+
+
 // Configuring the base URL for Axios requests
 axios.defaults.baseURL = "http://localhost:5000/api";
 
 /**
  * Axios request interceptor to include the authorization token in the headers.
  */
-axios.interceptors.request.use(config => {
+axios.interceptors.request.use((config) => {
   const token = store.commonStore.token;
-  if(token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+  if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
@@ -32,28 +34,36 @@ axios.interceptors.request.use(config => {
  * Axios response interceptor to introduce a delay and handle common HTTP errors.
  */
 axios.interceptors.response.use(async (response) => {
-  return response;
-}, (error: AxiosError) => {
-  const { data, status } = error.response!;
-  switch (status) {
-    case 400:
-      toast.error('Bad Request');
-      break;
-    case 401:
-      toast.error('Unauthorized');
-      break;
-    case 403:
-      toast.error('Forbidden');
-      break;
-    case 404:
-      toast.error('Not Found');
-      break;
-    case 500:
-      toast.error('Server Error');
-      break;
-  }
-  return Promise.reject(error);
+  return sleep(500)
+    .then(() => {
+      return response;
+    })
+    .catch((error) => {
+      console.log(error);
+      return Promise.reject(error);
+    });
 });
+
+// , (error: AxiosError) => {
+//   const { data, status } = error.response!;
+//   switch (status) {
+//     case 400:
+//       toast.error('Bad Request');
+//       break;
+//     case 401:
+//       toast.error('Unauthorized');
+//       break;
+//     case 403:
+//       toast.error('Forbidden');
+//       break;
+//     case 404:
+//       toast.error('Not Found');
+//       break;
+//     case 500:
+//       toast.error('Server Error');
+//       break;
+//   }
+//   return Promise.reject(error)
 
 /**
  * Function to extract the response data from an Axios response.
@@ -72,8 +82,7 @@ const requests = {
     axios.post<T>(url, body).then(responseBody),
   put: <T>(url: string, body: {}): Promise<T> =>
     axios.put<T>(url, body).then(responseBody),
-  del: <T>(url: string): Promise<T> =>
-    axios.delete<T>(url).then(responseBody),
+  del: <T>(url: string): Promise<T> => axios.delete<T>(url).then(responseBody),
 };
 
 /**
@@ -81,11 +90,14 @@ const requests = {
  */
 const Activities = {
   list: (): Promise<Activity[]> => requests.get<Activity[]>("/activities"),
-  details: (id: string): Promise<Activity> => requests.get<Activity>(`/activities/${id}`),
-  create: (activity: Activity): Promise<void> => requests.post<void>("/activities", activity),
+  details: (id: string): Promise<Activity> =>
+    requests.get<Activity>(`/activities/${id}`),
+  create: (activity: Activity): Promise<void> =>
+    requests.post<void>("/activities", activity),
   update: (activity: Activity): Promise<void> =>
     requests.put<void>(`/activities/${activity.id}`, activity),
-  delete: (id: string): Promise<void> => requests.del<void>(`/activities/${id}`),
+  delete: (id: string): Promise<void> =>
+    requests.del<void>(`/activities/${id}`),
 };
 
 /**
@@ -93,7 +105,8 @@ const Activities = {
  */
 const Account = {
   current: (): Promise<User> => requests.get<User>("/account"),
-  login: (user: UserFormValues): Promise<User> => requests.post<User>("/account/login", user),
+  login: (user: UserFormValues): Promise<User> =>
+    requests.post<User>("/account/login", user),
   register: (user: UserFormValues): Promise<User> =>
     requests.post<User>("/account/register", user),
 };
@@ -108,7 +121,7 @@ const Posts = {
   update: (post: Post): Promise<void> =>
     requests.put<void>(`/post/${post.id}`, post),
   delete: (id: string): Promise<void> => requests.del<void>(`/post/${id}`),
-}
+};
 
 /**
  * Main agent object for all objects.
