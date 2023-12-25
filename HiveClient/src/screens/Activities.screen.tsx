868f3710eb625d38ac03d5cf623683d9afbd601a
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-25 19:31:16 +0200

    Finishing the CRUD operations and api

--- HiveClient/src/screens/Activities.screen.tsx
+++ HiveClient/src/screens/Activities.screen.tsx
@@ -1,95 +1,39 @@
-import React, { useState, useEffect } from "react";
-import { Activity } from "../app/models/activity.ts";
+import React, { useEffect } from "react";
 import ActivityDashBoard from "../components/ActivityDashboard/ActivityDashBoard.tsx";
 import HiveGlobal from "../styled/HiveGlobal.styled.js";
 import LeftSection from "../components/LeftSection/LeftSection.component.jsx";
-import { v4 as uuid } from 'uuid';
-import agent from "../app/api/agent.ts";
 import { useStore } from "../app/stores/store.ts";
 import { observer } from "mobx-react-lite";
 import ActivitynavComponent from "../components/ActivityNavBar/Activitynav.component.tsx";
-import axios from "axios";
-
-
-interface Props {
-  activity: Activity | undefined;
-  openForm: () => void;
-}
+import Loader from "../components/Loader/Loader.component.jsx";
 
 /**
  * React component representing the main Activities page.
  * @function Activities
  * @returns {JSX.Element} The JSX representation of the Activities page.
  */
-function Activities({ activity, openForm }: Props): JSX.Element {
-  const {activityStore} = useStore();
-
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [submitting, setSubmitting] = useState(false);
-  const [editMode, setEditMode] = useState(false);
+function Activities(): JSX.Element {
+  const { activityStore } = useStore();
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data)
-    })
-  }, []);
+    activityStore.loadActivities();
+  }, [activityStore]);
 
-  function handleSelectActivity(id: string){
-    setSelectedActivity(activities.find(x => x.id === id));
-  }
-
-  function handleCancleSelectActivity(){
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string){
-    id ? handleSelectActivity(id) : handleCancleSelectActivity();
-    setEditMode(true);
-  }
-
-  function handleFormClose(){
-    setEditMode(false);
-  }
-
-
-  function handleCreateOrEditActivity(activity: Activity){
-    activity.id 
-      ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-      : setActivities([...activities, {...activity, id: uuid()}]);
-    setEditMode(false);
-    setSelectedActivity(activity);
-  }
-
-  function handleDeleteActivity(id: string){
-    setActivities([...activities.filter(x => x.id !== id)])
-  }
+  if (activityStore.loadingInitial) return <Loader />;
 
   return (
     <>
-    
       <HiveGlobal />
       <main className="main-hive">
-        <ActivitynavComponent openForm={handleFormOpen} />
+        <ActivitynavComponent/>
 
         <div className="hive-container">
           <LeftSection />
-          <ActivityDashBoard
-            activities={activities}
-            selectedActivity={selectedActivity}
-            selectActivity={handleSelectActivity}
-            cancelSelectActivity={handleCancleSelectActivity}
-            editMode={editMode}
-            openForm={handleFormOpen}
-            closeForm={handleFormClose}
-            createOrEdit={handleCreateOrEditActivity}
-            deleteActivity={handleDeleteActivity}
-            submitting={submitting}
-          />
+          <ActivityDashBoard/>
         </div>
       </main>
     </>
   );
 }
 
-export default observer(Activities); 
+export default observer(Activities);
