71b1509c8a9847924b1d60549fd8a6be065b961e
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-26 21:46:27 +0200

    Predefined options for event categories

---
+++ HiveClient/src/common/options/categoryOptions.ts
@@ -0,0 +1,39 @@
+/**
+ * Predefined options for event categories.
+ * Each option is an object with a 'text' representing the display text and a 'value' representing the category value.
+ * @const {Array<{ text: string, value: string }>}
+ * @memberof Constants
+ */
+
+export const categoryOptions = [
+    { text: 'Hackathon', value: 'hackathon' },
+    { text: 'Coding Workshop', value: 'coding_workshop' },
+    { text: 'Tech Conference', value: 'tech_conference' },
+    { text: 'Webinar', value: 'webinar' },
+    { text: 'Programming Contest', value: 'programming_contest' },
+    { text: 'Meetup', value: 'meetup' },
+    { text: 'Developer Summit', value: 'developer_summit' },
+    { text: 'Code Camp', value: 'code_camp' },
+    { text: 'Open Source Contribution', value: 'open_source_contribution' },
+    { text: 'IT Expo', value: 'it_expo' },
+    { text: 'Machine Learning Symposium', value: 'ml_symposium' },
+    { text: 'Data Science Meetup', value: 'data_science_meetup' },
+    { text: 'AI Workshop', value: 'ai_workshop' },
+    { text: 'Game Development Workshop', value: 'game_dev_workshop' },
+    { text: 'Cybersecurity Conference', value: 'cybersecurity_conference' },
+    { text: 'Blockchain Meetup', value: 'blockchain_meetup' },
+    { text: 'Cloud Computing Summit', value: 'cloud_computing_summit' },
+    { text: 'DevOps Training', value: 'devops_training' },
+    { text: 'UI/UX Design Hackathon', value: 'ui_ux_hackathon' },
+    { text: 'Robotics Expo', value: 'robotics_expo' },
+    { text: 'Embedded Systems Symposium', value: 'embedded_systems_symposium' },
+    { text: 'AR/VR Showcase', value: 'ar_vr_showcase' },
+    { text: 'Big Data Analytics Seminar', value: 'big_data_analytics_seminar' },
+    { text: 'Drinks', value: 'drinks' },
+    { text: 'Culture', value: 'culture' },
+    { text: 'Film', value: 'film' },
+    { text: 'Food', value: 'food' },
+    { text: 'Music', value: 'music' },
+    { text: 'Travel', value: 'travel' },
+  ];
+  
