b25b03f31ea7c07da343432a4e5fee977596eedb
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-27 23:17:49 +0200

    A React component for filtering and displaying activities.

--- HiveClient/src/components/ActivityDashboard/ActivityFilters.jsx
+++ HiveClient/src/components/ActivityDashboard/ActivityFilters.jsx
@@ -3,12 +3,15 @@ import { Header, Menu } from 'semantic-ui-react';
 import "semantic-ui-css/semantic.min.css";
 import Calendar from 'react-calendar';
 
-
+/**
+ * A React component for filtering and displaying activities.
+ * @component
+ */
 function ActivityFilters() {
   return (
     <>
         <Menu vertical size='large' style={{width: '100%', marginTop:'1rem'}}>
-            <Header icon='filter' attached color='teal' content='Filters' />
+            <Header icon='filter' attached color='orange' content='Filters' />
             <Menu.Item content='All Activities' />
             <Menu.Item content="I'm going" />
             <Menu.Item content="I'm hosting" />
