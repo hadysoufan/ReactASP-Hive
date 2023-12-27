3a604bb353c95aec1ac03c8805f018479dcc8494
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-27 23:18:22 +0200

    A React component for displaying and interacting with comments related to an activity.

--- HiveClient/src/components/ActivityDetails/ActivityDetailedChat.tsx
+++ HiveClient/src/components/ActivityDetails/ActivityDetailedChat.tsx
@@ -1,64 +1,108 @@
-import React from 'react'
-import {Segment, Header, Comment, Form, Button} from 'semantic-ui-react'
+import React, { useEffect } from "react";
+import { Segment, Header, Comment, Loader } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
-import { observer } from 'mobx-react-lite';
+import { observer } from "mobx-react-lite";
 import Profile1 from "../../asset/img/hive/profile-1.jpg";
+import { useStore } from "../../app/stores/store.ts";
+import { Link } from "react-router-dom";
+import { Formik, Form, Field, FieldProps } from "formik";
+import * as Yup from "yup";
+import { formatDistanceToNow } from "date-fns";
 
-function ActivityDetailedChat() {
+interface Props {
+  activityId: string;
+}
+
+/**
+ * A React component for displaying and interacting with comments related to an activity.
+ * @component
+ * @param {Props} props - The component properties.
+ * @param {string} props.activityId - The unique identifier for the activity.
+ */
+function ActivityDetailedChat({ activityId }: Props) {
+  const { commentStore } = useStore();
+
+  useEffect(() => {
+    if (activityId) {
+      commentStore.createHubConnection(activityId);
+    }
+
+    return () => {
+      commentStore.clearComments();
+    };
+  }, [commentStore, activityId]);
+
   return (
     <>
       <Segment
-                textAlign='center'
-                attached='top'
-                inverted
-                color='teal'
-                style={{border: 'none'}}
-            >
-                <Header>Chat about this event</Header>
-            </Segment>
-            <Segment attached>
-                <Comment.Group>
-                    <Comment>
-                        <Comment.Avatar src={Profile1}/>
-                        <Comment.Content>
-                            <Comment.Author as='a'>Matt</Comment.Author>
-                            <Comment.Metadata>
-                                <div>Today at 5:42PM</div>
-                            </Comment.Metadata>
-                            <Comment.Text>How artistic!</Comment.Text>
-                            <Comment.Actions>
-                                <Comment.Action>Reply</Comment.Action>
-                            </Comment.Actions>
-                        </Comment.Content>
-                    </Comment>
-
-                    <Comment>
-                        <Comment.Avatar src={Profile1}/>
-                        <Comment.Content>
-                            <Comment.Author as='a'>Joe Henderson</Comment.Author>
-                            <Comment.Metadata>
-                                <div>5 days ago</div>
-                            </Comment.Metadata>
-                            <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
-                            <Comment.Actions>
-                                <Comment.Action>Reply</Comment.Action>
-                            </Comment.Actions>
-                        </Comment.Content>
-                    </Comment>
-
-                    <Form reply>
-                        <Form.TextArea/>
-                        <Button
-                            content='Add Reply'
-                            labelPosition='left'
-                            icon='edit'
-                            primary
-                        />
-                    </Form>
-                </Comment.Group>
-            </Segment>
+        textAlign="center"
+        attached="top"
+        inverted
+        color="orange"
+        style={{ border: "none" }}
+      >
+        <Header>Chat about this event</Header>
+      </Segment>
+      <Segment clearing>
+        <Formik
+          onSubmit={(values, { resetForm }) =>
+            commentStore.addComment(values).then(() => resetForm())
+          }
+          initialValues={{ body: "" }}
+          validationSchema={Yup.object({
+            body: Yup.string().required(),
+          })}
+        >
+          {({ isSubmitting, isValid, handleSubmit }) => (
+            <Form className="ui form">
+              <Field name="body">
+                {(props: FieldProps) => (
+                  <div style={{ position: "relative" }}>
+                    <Loader active={isSubmitting} />
+                    <textarea
+                      placeholder="Enter your comment"
+                      rows={2}
+                      {...props.field}
+                      onKeyPress={(e) => {
+                        if (e.key === "Enter" && e.shiftKey) {
+                          return;
+                        }
+                        if (e.key === "Enter" && !e.shiftKey) {
+                          e.preventDefault();
+                          isValid && handleSubmit();
+                        }
+                      }}
+                    />
+                  </div>
+                )}
+              </Field>
+            </Form>
+          )}
+        </Formik>
+        <Comment.Group>
+          {commentStore.comments.map((comment) => (
+            <Comment key={comment.id}>
+              <Comment.Avatar src={comment.image || Profile1} />
+              <Comment.Content>
+                <Comment.Author
+                  as={Link}
+                  to={`/hive/user-profile/${comment.username}`}
+                >
+                  {comment.displayName}
+                </Comment.Author>
+                <Comment.Metadata>
+                  <div>{formatDistanceToNow(comment.createdAt)} ago</div>
+                </Comment.Metadata>
+                <Comment.Text style={{ whiteSpace: "pre-wrap" }}>
+                  {comment.body}
+                </Comment.Text>
+              </Comment.Content>
+            </Comment>
+          ))}
+        </Comment.Group>
+      </Segment>
     </>
-  )
+  );
 }
 
 export default observer(ActivityDetailedChat);
