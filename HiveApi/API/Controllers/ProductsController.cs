edea7f9f179411be567e2ec56a8bbb7b4143bd58
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-29 19:05:20 +0200

    Product controller

---
+++ HiveApi/API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(Guid id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
