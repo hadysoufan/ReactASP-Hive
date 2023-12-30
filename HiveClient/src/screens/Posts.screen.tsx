a23ba7a5a3c2dec1d5ffc2fc6dad76e2801e2feb
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-30 22:15:57 +0200

    Finishing all code for Post

--- HiveClient/src/screens/Posts.screen.tsx
+++ HiveClient/src/screens/Posts.screen.tsx
@@ -1,28 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PostDashBoard from "../components/PostDashboard/PostDashBoard.tsx";
 import Loader from "../components/Loader/Loader.component.jsx";
 import { useStore } from "../app/stores/store.ts";
 import { observer } from "mobx-react-lite";
+import axios, { AxiosResponse } from "axios";
+import { Photo } from "../app/models/photos.ts";
 
 /**
  * A React component for displaying posts.
  * @component
  */
 function Posts() {
-
-  const {postStore} = useStore();
-
+  const [photos, setPhotos] = useState<Photo[]>([]); 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    postStore.loadPosts();
-  }, [postStore]);
+    axios
+      .get<Photo[]>('http://localhost:5000/api/photo')
+      .then((response: AxiosResponse<Photo[]>) => {
+        setPhotos(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false); 
+      });
+  }, []); 
 
-  
-  if(postStore.loadingInitial) <Loader />
+  if (loading) return <Loader />;
 
   return (
     <>
-      <PostDashBoard />
+      <PostDashBoard photos={photos} />
     </>
   );
 }
