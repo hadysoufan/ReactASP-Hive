355fd317a6a4e957fb837a87afac6cbad8ef2d1a
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-30 22:08:48 +0200

    Adding all CRUD opt

--- HiveApi/API/Controllers/PhotoController.cs
+++ HiveApi/API/Controllers/PhotoController.cs
@@ -1,5 +1,8 @@
 ﻿using Application.Photos;
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 
 namespace API.Controllers
 {
@@ -10,13 +13,40 @@ namespace API.Controllers
     [ApiController]
     public class PhotoController : BaseApiController
     {
+        private readonly DataContext _context;
+
+        public PhotoController(DataContext context)
+        {
+            _context = context;
+        }
+
         /// <inheritdoc />
         // Post: api/photo
-        [HttpPost]
+        [HttpPost("create-photo")]
         public async Task<IActionResult> Add([FromForm] Add.Command command)
         {
             return HandleResult(await Mediator.Send(command));
         }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Photo>>> GetPhotos()
+        {
+            return await _context.Photos.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Photo>> GetPhoto(string id)
+        {
+            var photo = await _context.Photos.FindAsync(id);
+
+            if (photo is null)
+            {
+                return NotFound();
+            }
+
+            return photo;
+        }
+
 
         /// <inheritdoc />
         // Post: api/photo/{id}
@@ -24,6 +54,13 @@ namespace API.Controllers
         public async Task<IActionResult> Delete(string id)
         {
             return HandleResult(await Mediator.Send(new Delete.Command{ Id = id}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditPhoto(string id, Photo photo)
+        {
+            photo.Id = id;
+            return HandleResult(await Mediator.Send(new Edit.Command { Photo = photo }));
         }
 
         /// <inheritdoc />
