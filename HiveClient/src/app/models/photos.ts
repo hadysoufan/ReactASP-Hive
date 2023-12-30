dacec7f1a13c839a2baacc79574158245545432e
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-30 22:13:05 +0200

    Creating photo modal

---
+++ HiveClient/src/app/models/photos.ts
@@ -0,0 +1,22 @@
+export interface Photo {
+  id: string;
+  url: string;
+  isMain: boolean;
+  owner: string;
+  description: string;
+  date: string ;
+}
+
+export class PhotoFormValues {
+  id?: string = undefined;
+  description: string = "";
+  url: string = '';
+
+  constructor(photo?: PhotoFormValues) {
+    if (photo) {
+      this.id = photo.id;
+      this.description = photo.description;
+      this.url = photo.url;
+    }
+  }
+}
